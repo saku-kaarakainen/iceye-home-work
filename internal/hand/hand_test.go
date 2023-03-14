286bf52d8ed1ee578594d27cddab4233ce28083c
Saku Kaarakainen <sakuba91@hotmail.com>
2023-03-15 01:27:09 +0200

    refactor testname

--- internal/hand/hand_test.go
+++ internal/hand/hand_test.go
@@ -2,7 +2,7 @@ package hand
 
 import "testing"
 
-func TestIsFourOfKind_OK(t *testing.T) {
+func TestMethods(t *testing.T) {
 	vals := map[rune]int{
 		'A': 14, 'K': 13, 'Q': 12, 'J': 11, 'T': 10, '9': 9,
 		'8': 8, '7': 7, '6': 6, '5': 5, '4': 4, '3': 3, '2': 2,
